ad11df174987801a0d15be5407762290926e6569
shitongtan <shi_tong_tan@hotmail.com>
2019-10-23 01:02:51 +0800

    Title

---
+++ app/src/main/java/com/example/the_hawks/HawkerStall.java
@@ -0,0 +1,19 @@
+package com.example.the_hawks;
+
+public class HawkerStall {
+    //constructor
+    public HawkerStall(){
+        String stallName;
+        float hygieneRating;
+        int demeritPoints;
+        String address;
+        float googleRating;
+    }
+
+    public float getHygieneRating() {return hygieneRating;}
+    public int getDemeritPoints() {return demeritPoints;}
+    public String getAddress() {return address;};
+    public float getGoogleRating(){return googleRating;}
+    public String getStallName() {return stallName;}
+
+}
